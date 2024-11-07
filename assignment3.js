720c6331dd9261c020a4b347b48e025d028f8f48
nabin893 <nayeemkhondoker@gmail.com>
2024-11-07 19:29:49 +0600

    push

---
+++ assignment3.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+for(let num = 5; num<=50; num +=5){
+    console.log(num);
+    
+
+}
---
+++ assignment4.js
@@ -0,0 +1,29 @@
+
+
+                // tipe 1
+
+    for(let num = 21; num >= 15; num -= 1){
+       
+            console.log(num);
+
+    }
+
+
+
+                //    tipe 2
+
+
+
+
+ let names = [15,16,17,18,19,20,21];
+
+ console.log( names.reverse().join(" "));
+
+   
+
+
+
+
+
+
+
