0cce9052d237be6f02e974d6d59b4f1c46e104d7
nabin893 <nayeemkhondoker@gmail.com>
2024-11-07 16:20:50 +0600

    push

---
+++ assignment5.js
@@ -0,0 +1,14 @@
+
+
+
+let num = 500;
+let newnum = 0;
+
+for (let i = 0; i <= num; i++) {
+
+    if (i % 3 == 0 && i % 15 == 0) {
+        // console.log(i);
+    }
+    newnum = newnum + i;
+}
+console.log( newnum);
